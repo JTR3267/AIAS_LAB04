7d5754f2d538b7113df04efb8a3dbc8626d6b342
jimmyii <jimmyliu.ii12@nycu.edu.tw>
2024-07-29 20:35:47 +0800

    TA

---
+++ src/main/scala/acal_lab04/Hw4/Queue.scala
@@ -0,0 +1,30 @@
+package acal_lab04.Lab
+
+import chisel3._
+import chisel3.util.log2Ceil
+
+class Queue(val depth: Int) extends Module {
+  val io = IO(new Bundle {
+    val push    = Input(Bool())
+    val pop     = Input(Bool())
+    val en      = Input(Bool())
+    val dataIn  = Input(UInt(32.W))
+    val dataOut = Output(UInt(32.W))
+  })
+
+  val queue_mem = Mem(depth, UInt(32.W))
+  val head      = RegInit(0.U(log2Ceil(depth+1).W))
+  val tail      = RegInit(0.U(log2Ceil(depth+1).W))
+  val out       = RegInit(0.U(32.W))
+
+  when (io.en) {
+    when(io.push) {
+      queue_mem(tail) := io.dataIn
+      tail := tail + 1.U
+    }.elsewhen(io.pop && (tail > head)) {
+      out := queue_mem(head)
+      head := head + 1.U
+    }
+  }
+  io.dataOut := out
+}
---
+++ src/test/scala/acal_lab04/Hw4/QueueTest.scala
@@ -0,0 +1,40 @@
+package acal_lab04.Lab
+
+import chisel3.iotesters.{PeekPokeTester, Driver, ChiselFlatSpec}
+import scala.collection.mutable.{Queue => ScalaQueue}
+
+class QueueTests(c: Queue) extends PeekPokeTester(c) {
+  var nxtDataOut = 0
+  var dataOut = 0
+  val queue = new ScalaQueue[Int]()
+
+  for (t <- 0 until 16) {
+    println(s"Tick $t")
+    val enable  = rnd.nextInt(2)
+    val push    = rnd.nextInt(2)
+    val pop     = rnd.nextInt(2)
+    val dataIn  = rnd.nextInt(256)
+    println(s"enable $enable push $push pop $pop dataIn $dataIn")
+    if (enable == 1) {
+      dataOut = nxtDataOut
+      if (push == 1) {
+        queue.enqueue(dataIn)
+      } else if (pop == 1 && queue.length > 0) {
+        nxtDataOut = queue.dequeue()
+      }
+    }
+
+    poke(c.io.pop,    pop)
+    poke(c.io.push,   push)
+    poke(c.io.en,     enable)
+    poke(c.io.dataIn, dataIn)
+    step(1)
+    expect(c.io.dataOut, dataOut)
+  }
+}
+
+object QueueTest extends App {
+  Driver.execute(Array("-td","./generated","-tbn","verilator"),() => new Queue(8)) {
+    c => new QueueTests(c)
+  }
+}
