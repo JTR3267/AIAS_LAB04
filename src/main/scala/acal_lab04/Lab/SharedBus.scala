b7593654e9d491f0ebeb8d9ec483c440d5a80ddd
jimmyii <jimmyliu.ii12@nycu.edu.tw>
2024-08-26 23:04:06 +0800

    initial

--- src/main/scala/acal_lab04/Lab/SharedBus.scala
+++ src/main/scala/acal_lab04/Lab/SharedBus.scala
@@ -10,14 +10,30 @@ class ShareBus( addrWidth: Int, dataWidth: Int, numMasters: Int, numSlaves: Int,
     val slaves = Vec(numSlaves, new SlaveInterface(addrWidth, dataWidth))
   })
     // Wire define
-    val masterReady = Wire(Bool())
-    val masterValid = Wire(Bool())
-    val masterAddr = Wire(UInt(addrWidth.W))
-    val masterData = Wire(UInt(dataWidth.W))
-    val masterSize = Wire(UInt(log2Ceil(dataWidth).W))
+    val masterReady = WireInit(false.B)
+    val masterValid = WireInit(false.B)
+    val masterAddr = WireInit(0.U(addrWidth.W))
+    val masterData = WireInit(0.U(dataWidth.W))
+    val masterSize = WireInit(0.U(log2Ceil(dataWidth).W))
     
     // decoder
     val decoder = Module(new Decoder(addrWidth, numSlaves, addrMap))
+    
+    // Initialize signals
+    for (i <- 0 until numSlaves) {
+      io.slaves(i).valid := false.B
+      io.slaves(i).addr  := 0.U
+      io.slaves(i).data  := 0.U
+      io.slaves(i).size  := 0.U
+      io.slaves(i).ready := true.B
+    }
+    for (i <- 0 until numMasters){
+      io.masters(i).ready := false.B
+      io.masters(i).addr  := 0.U
+      io.masters(i).data  := 0.U
+      io.masters(i).size  := 0.U
+      io.masters(i).valid := false.B
+    }
 
     // Connect master address to decoder
     decoder.io.addr := io.masters(0).addr
@@ -29,20 +45,18 @@ class ShareBus( addrWidth: Int, dataWidth: Int, numMasters: Int, numSlaves: Int,
     // Connect master to wire
     io.masters(0).ready := io.slaves.map(_.ready).reduce(_ || _)
     masterValid := io.masters(0).valid
-    masterAddr := io.masters(0).addr
-    masterData := io.masters(0).data
-    masterSize := io.masters(0).size
-
+    masterAddr  := io.masters(0).addr
+    masterData  := io.masters(0).data
+    masterSize  := io.masters(0).size
+    
     // Connect slave to wire
-    io.slaves(slaveSelect).valid := masterValid
-    io.slaves(slaveSelect).addr := masterAddr
-    io.slaves(slaveSelect).data := masterData
-    io.slaves(slaveSelect).size := masterSize
-
-    // Return ready to Master
-    for (i <- 0 until numSlaves) {
-        io.masters(0).ready := io.slaves.map(_.ready).reduce(_ || _)
+    when(masterValid) {
+      io.slaves(slaveSelect).valid := masterValid
+      io.slaves(slaveSelect).addr  := masterAddr
+      io.slaves(slaveSelect).data  := masterData
+      io.slaves(slaveSelect).size  := masterSize
     }
+
 }
 
 class MasterInterface(addrWidth: Int, dataWidth: Int) extends Bundle {
