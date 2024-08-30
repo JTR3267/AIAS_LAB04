0109eb7073724d9065f0bd0901b4f595bbe985c6
jimmyliu <jimmyliu.ii12@nycu.edu.tw>
2024-08-30 15:38:27 +0000

    2024_Fall_Queue_SharedBus

--- src/main/scala/acal_lab04/Hw4/Queue.scala
+++ src/main/scala/acal_lab04/Hw4/Queue.scala
@@ -14,21 +14,5 @@ class Queue(val depth: Int) extends Module {
     val full    = Output(Bool())
   })
 
-  val queue_mem = Mem(depth, UInt(32.W))
-  val head      = RegInit(0.U(log2Ceil(depth+1).W))
-  val tail      = RegInit(0.U(log2Ceil(depth+1).W))
-  val out       = RegInit(0.U(32.W))
 
-  when (io.en) {
-    when(io.push) {
-      queue_mem(tail) := io.dataIn
-      tail := tail + 1.U
-    }.elsewhen(io.pop && (tail > head)) {
-      out := queue_mem(head)
-      head := head + 1.U
-    }
-  }
-  io.dataOut := out
-  io.empty := head === tail
-  io.full := tail - head === depth.asUInt
 }
--- src/main/scala/acal_lab04/Hw5/MulitSharedBus.scala
+++ src/main/scala/acal_lab04/Hw5/MulitSharedBus.scala
@@ -10,28 +10,5 @@ class MultiShareBus(val addrWidth: Int,val dataWidth: Int,val numMasters: Int,va
     val masters = Vec(numMasters, Flipped(Decoupled(new MasterInterface(addrWidth, dataWidth))))
     val slaves = Vec(numSlaves, Decoupled(new SlaveInterface(addrWidth, dataWidth)))
   })
-    // decoder
-    val decoders = Seq.tabulate(numSlaves) { i =>
-      Module(new Decoder(addrWidth, addrMap.slice(i, i+1)))
-    }
-    // arbiter
-    val arbiter = Module(new Arbiter(new MasterInterface(addrWidth, dataWidth),numMasters))
-
-    // Connect masters to arbiter
-    arbiter.io.in <> io.masters
-
-    // Connect arbiter to slaves
-    val select = arbiter.io.out.bits.addr
-    val chosen = arbiter.io.chosen
-    arbiter.io.out.ready := io.slaves.map(_.ready).reduce(_ || _) // OR all ready signals
-
-    // Initialize signals
-    for (i <- 0 until numSlaves) {
-      io.slaves(i).valid := io.masters(chosen).valid && decoders(i).io.select
-      io.slaves(i).bits.addr  := io.masters(chosen).bits.addr
-      io.slaves(i).bits.data  := io.masters(chosen).bits.data
-      io.slaves(i).bits.size  := io.masters(chosen).bits.size
-      decoders(i).io.addr := io.masters(chosen).bits.addr
-    }
 
 }
